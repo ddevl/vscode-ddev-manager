da64e45371c42f1e5659f2835f3bc41525fb668f
biati-digital <manuel@biati.digital>
2026-07-08 19:46:34 -0600

    chore: removed unused code

--- webpack.config.js
+++ webpack.config.js
@@ -72,10 +72,6 @@ const extensionConfig = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src', 'websocket', 'server.js'),
-          to: "websocket/"
-        },
-        {
           from: path.resolve(__dirname, 'includes'),
           to: path.resolve(__dirname, 'dist', 'includes'),
         }
@@ -84,21 +80,4 @@ const extensionConfig = {
   ],
 };
 
-
-// Config for webview source code (to be run in a web-based context)
-/** @type WebpackConfig */
-const webviewConfig = {
-  ...baseConfig,
-  target: ["web", "es2020"],
-  entry: "./src/webview/main.ts",
-  experiments: { outputModule: true },
-  output: {
-    path: path.resolve(__dirname, "out"),
-    filename: "webview.js",
-    libraryTarget: "module",
-    chunkFormat: "module",
-  },
-};
-
-// module.exports = [extensionConfig, webviewConfig, serverConfig];
-module.exports = [extensionConfig, webviewConfig];
+module.exports = [extensionConfig];
